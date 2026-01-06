2c3d1ce1ad6485b378bb00b88eb5920d278be7a1
Baltazar <pltka1235@gmail.com>
2026-01-06 17:18:37 +0100

    treti commit

---
+++ snake/cmake-build-debug/CMakeFiles/server.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Baltazar/CLionProjects/Csnake/snake/common/game.c" "CMakeFiles/server.dir/common/game.c.obj" "gcc" "CMakeFiles/server.dir/common/game.c.obj.d"
+  "C:/Users/Baltazar/CLionProjects/Csnake/snake/common/ipc.c" "CMakeFiles/server.dir/common/ipc.c.obj" "gcc" "CMakeFiles/server.dir/common/ipc.c.obj.d"
+  "C:/Users/Baltazar/CLionProjects/Csnake/snake/common/world.c" "CMakeFiles/server.dir/common/world.c.obj" "gcc" "CMakeFiles/server.dir/common/world.c.obj.d"
+  "C:/Users/Baltazar/CLionProjects/Csnake/snake/server/server.c" "CMakeFiles/server.dir/server/server.c.obj" "gcc" "CMakeFiles/server.dir/server/server.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
